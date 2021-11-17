7919abd391d51b7945dcb1580190ae3891d9fdbd
Alexey Nikitin <anikitin@eastbanctech.com>
2021-11-17 11:20:34 +0700

    initial template generated file + solution

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using net6_base_angular_app.Models;
+
+namespace net6_base_angular_app.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+        
+    }
+}
